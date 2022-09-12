42f57d18e34566de7203863b4e25c8bfbcbcdd55
Lughas Onyebuchi <lughasonyebuchi@gmail.com>
2022-09-12 03:33:47 -0700

    Print Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int a;
+
+for (a = 0; a <= 9; a++)
+{
+putchar((a % 10) + '0');
+if (a == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
