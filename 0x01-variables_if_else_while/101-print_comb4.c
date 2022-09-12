5f408fd37852321ab2196c14ba58d99abd04ca82
Lughas Onyebuchi <lughasonyebuchi@gmail.com>
2022-09-12 12:21:21 -0700

    Print Combination 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-int d = 012, a = 012, i = 012;
+int digit1, digit2, digit3;
 
-while (d < 790)
+for (digit1 = 0; digit1 < 8; digit1++)
 {
-a = 012;
-while (a < 790)
+for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 {
-i = 012;
-while (i < 790)
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 {
-if (d != a && a != i && d != i && d < a && a < i)
-{
-putchar(d);
-putchar(a);
-putchar(i);
-if (!(d == 012 && a == 013 && i == 014))
-{
-						putchar(',');
-						putchar(' ');
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+putchar((digit3 % 10) + '0');
+
+if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+continue;
+
+putchar(',');
+putchar(' ');
 }
 }
-i++;
 }
-a++;
-}
-d++;
-}
+
 putchar('\n');
 
 return (0);
