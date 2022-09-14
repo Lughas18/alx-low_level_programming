6cc39b0b60cbc214d713dc74510e085fb0088981
Lughas Onyebuchi <lughasonyebuchi@gmail.com>
2022-09-14 00:52:48 -0700

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('r');
+	putchar('i');
+	putchar('n');
+	putchar('t');
+	putchar('t');
+	putchar('\n');
+
+	return (0);
+}
