84047f70d8278f5733cf4ec8644d968632a6c4a2
Lughas Onyebuchi <lughasonyebuchi@gmail.com>
2022-09-21 05:39:36 -0700

    Checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _isdigit - checks for a digit (0 through 9)
+ *
+ *   * @c: carrier integer variable
+ *
+ *    * Return: 1 if true, 0 is false
+ *
+ */
+
+
+
+int _isdigit(int c)
+
+{
+
+if (c >= 48 && c <= 57)
+
+{
+
+return (1);
+}
+
+return (0);
+
+}
