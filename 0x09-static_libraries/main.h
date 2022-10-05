43c49f4f627a2828c3ddeb96c765ee1095e9fca0
Lughas Onyebuchi <lughasonyebuchi@gmail.com>
2022-10-05 08:06:42 -0700

    Static Libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -44,6 +44,18 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
+void _puts_recursion(char *s);
 
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
 
 #endif /* MAIN_H */
